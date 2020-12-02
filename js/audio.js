8c1cf847651fd9d13e08de1e60cfaa32ff1c2e41
Vera Adiele <veralee225@gmail.com>
2020-12-02 13:52:04 +0100

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -11,27 +11,4 @@ function setHalfVolume() {
 alert("Welcome! Enjoy your experince");
 
 
-window.addEventListener("DOMcontentLoaded", event => {
-    let audio = document.querySelector("audio");
-    audio.volume = 0.2;
-    audio.play();
-});
 
-/*const button = document.querySelector("#button");
-const icon = document.querySelector("#button > i");
-const audio = document.querySelector("audio");
-
-button.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.volume = 0.2;
-    audio.play();
-    icon.classList.remove('fa-volume-up');
-    icon.classList.add('fa-volume-mute');
-    
-  } else {
-    audio.pause();
-    icon.classList.remove('fa-volume-mute');
-    icon.classList.add('fa-volume-up');
-  }
-  button.classList.add("fade");
-}); */
